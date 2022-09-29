e0b48bdcbbabde716fa7edf50cb3f7ea4a42348a
AtomicFeasT <atomic-feast@hotmail.com>
2022-09-29 07:15:08 -0300

    Added base and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  base: "/MiniPort/",
+  site: "https://atomicfeast.github.io/MiniPort/",
+})
