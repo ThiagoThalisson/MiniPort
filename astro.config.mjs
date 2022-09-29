70f5e240fd2a327bf8d2658325e9dc72eba6215d
AtomicFeasT <atomic-feast@hotmail.com>
2022-09-29 07:30:23 -0300

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  base: "MiniPort/",
   site: "https://atomicfeast.github.io/MiniPort/",
 })
