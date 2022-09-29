1ec85be8f7fc7d58ed69d21ed7c80aa3e93da2c5
AtomicFeasT <atomic-feast@hotmail.com>
2022-09-29 08:36:13 -0300

    Fixed links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   base: "MiniPort/",
-  site: "https://atomicfeast.github.io/MiniPort/",
 })
